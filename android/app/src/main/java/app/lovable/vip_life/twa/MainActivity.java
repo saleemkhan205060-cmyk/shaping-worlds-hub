ca11a343215b4783a2a5fd54e5ab1dcc4286a068
saleemkhan205060-cmyk <saleemkhan205060@gmail.com>
2026-08-21 10:20:18 +0300

    Update MainActivity.java

--- android/app/src/main/java/app/lovable/vip_life/twa/MainActivity.java
+++ android/app/src/main/java/app/lovable/vip_life/twa/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends BridgeActivity implements ModifiedMainActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         registerPlugin(SafeBrowserPlugin.class);
-        registerPlugin(PushSupportPlugin.class);
+       // registerPlugin(PushSupportPlugin.class);
         // Initialize Firebase (when google-services.json is present) BEFORE any
         // push code can reach FirebaseMessaging.getInstance().
         PushSupportPlugin.ensureFirebase(this);
