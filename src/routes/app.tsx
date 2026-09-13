9b61c0df050d4803a6bc3aa5b3ceb721d7c61b1d
saleemkhan205060-cmyk <saleemkhan205060@gmail.com>
2026-09-13 09:57:42 +0300

    Refactor route to use async GET handler

--- src/routes/app.tsx
+++ src/routes/app.tsx
@@ -4,13 +4,16 @@ const PLAY_STORE_URL =
   "https://play.google.com/store/apps/details?id=app.lovable.vip_life.twa";
 
 export const Route = createFileRoute("/app")({
-  beforeLoad: () => {
-    throw new Response(null, {
-      status: 302,
-      headers: {
-        Location: PLAY_STORE_URL,
+  server: {
+    handlers: {
+      GET: async () => {
+        return new Response(null, {
+          status: 302,
+          headers: {
+            Location: PLAY_STORE_URL,
+          },
+        });
       },
-    });
+    },
   },
-  component: () => null,
 });
