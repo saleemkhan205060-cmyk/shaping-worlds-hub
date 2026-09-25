12f653a16b7a325deed52b6366840fdca36e2ab2
saleemkhan205060-cmyk <saleemkhan205060@gmail.com>
2026-09-25 17:40:38 +0300

    Increase logo image size in Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -230,7 +230,7 @@ export function Layout({
            <img
            src={logoUrl}
           alt="VIP Life logo"
-         className="h-11 w-11 object-contain"
+         className="h-13 w-13 object-contain"
         />
        </div>
           </div>
