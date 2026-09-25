52f9392e717a5c5a3d87dfd865c794a30138eb04
saleemkhan205060-cmyk <saleemkhan205060@gmail.com>
2026-09-25 23:38:15 +0300

    Update marriage.index.tsx

--- src/routes/marriage.index.tsx
+++ src/routes/marriage.index.tsx
@@ -391,7 +391,7 @@ useEffect(() => {
 
     <div>
   <p className="text-xs text-white/60">Income</p>
-  <p className="text-sm font-medium">{selected.pref_income ?? "Not added"}</p>
+  <p className="text-sm font-medium">{selected.income ?? "Not added"}</p>
 </div>
 
 <div>
